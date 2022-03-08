1bc62af8ea68a8989c38c184f0b52c5ed8262358
rvartak <rvartak@WMNCKAPV0001>
2022-03-08 06:06:46 -0500

    Add project files.

---
+++ mbia.price.pom_upload_reminders/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mbia.price.pom_upload_reminders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
